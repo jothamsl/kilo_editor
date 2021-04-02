3375da3f58978d2e2e4feb21da067c492c0c8d67
Jothamsl <jothamsl1@gmail.com>
2021-04-02 19:59:26 +0100

    Echo disabler function implementation

--- kilo.c
+++ kilo.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+void enableRawMode();
 
 int main()
 {
+    enableRawMode(); // Disables printing to terminal (Disables echo)
+
     char c;
-    while (read(STDERR_FILENO, &c, 1) == 1);
+    // Read function takes 1 byte input from stdio and returns number of bytes read
+    // It returns 0 when it reaches the end of a file
+    while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q'); 
     return 0;
+}
+
+void enableRawMode()
+{
+    struct termios raw; 
+
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
